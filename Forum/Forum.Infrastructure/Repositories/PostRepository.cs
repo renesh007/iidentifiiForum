8750a494548425353913dee7082ea19420807730
renesh007 <r.nandk@yahoo.com>
2025-08-31 11:08:32 +0200

    feat: skeleton implementation for infrastructure

---
+++ Forum/Forum.Infrastructure/Repositories/PostRepository.cs
@@ -0,0 +1,25 @@
+﻿using Forum.Domain.Entities;
+using Forum.Domain.Interfaces.Repository;
+
+namespace Forum.Infrastructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly IDbConnectionFactory _dbConnectionFactory;
+        public PostRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            _dbConnectionFactory = dbConnectionFactory;
+        }
+
+        public async Task<Guid> CreateAsync(string title, string content, Guid userId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Post?> GetByIdAsync(Guid postId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Forum/Forum.Infrastructure/Repositories/ViewPostRepository.cs
@@ -0,0 +1,19 @@
+﻿using Forum.Domain.Entities;
+using Forum.Domain.Interfaces.Repositories;
+using Forum.Domain.Models;
+
+namespace Forum.Infrastructure.Repositories
+{
+    internal class ViewPostRepository : IViewPostRepository
+    {
+        public async Task<PostView?> GetFullPostByIdAsync(Guid postId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<(IEnumerable<PostView>, int TotalCount)> GetPostsAsync(int pageNumber, int pageSize, FilterOptions? filterOptions, SortingDirection? sortingDirection, SortingOptions? sortingOptions, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
