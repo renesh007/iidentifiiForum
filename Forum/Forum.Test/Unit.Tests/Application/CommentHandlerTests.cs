777ae93ca01b9ffc0d783c6b176ff0408510c740
renesh007 <r.nandk@yahoo.com>
2025-08-31 20:14:13 +0200

    test: implement tag and comment handler tests

---
+++ Forum/Forum.Test/Unit.Tests/Application/CommentHandlerTests.cs
@@ -0,0 +1,67 @@
+﻿using Forum.Application.Exceptions;
+using Forum.Application.Handlers;
+using Forum.Domain.Interfaces.Repository;
+using NSubstitute;
+
+namespace Forum.Test.Unit.Tests.Application
+{
+    [TestFixture]
+    public class CommentHandlerTests
+    {
+        private ICommentRepository _commentRepository;
+        private CommentHandler _commentHandler;
+        private CancellationToken _cancellationToken;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _commentRepository = Substitute.For<ICommentRepository>();
+            _commentHandler = new CommentHandler(_commentRepository);
+            _cancellationToken = CancellationToken.None;
+        }
+
+        [Test]
+        public async Task GivenValidResult_WhenCreateCommentAsync_ThenShould_ReturnCommentId()
+        {
+            // Arrange
+            _commentRepository
+                .CreateCommentAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<Guid>(), _cancellationToken)
+                .Returns(0);
+
+            Guid postId = Guid.NewGuid();
+            Guid userId = Guid.NewGuid();
+
+            // Act
+            Guid result = await _commentHandler.CreateCommentAsync(postId, "Test Content", userId, _cancellationToken);
+
+            // Assert
+            Assert.AreNotEqual(Guid.Empty, result);
+        }
+
+        [Test]
+        public void GivenPostNotFound_WhenCreateCommentAsync_ThenShouldThrowPostNotFoundException()
+        {
+            // Arrange
+            _commentRepository
+                .CreateCommentAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<Guid>(), _cancellationToken)
+                .Returns(1001);
+
+            // Act & Assert
+            Assert.ThrowsAsync<PostNotFoundException>(() =>
+                _commentHandler.CreateCommentAsync(Guid.NewGuid(), "Test Content", Guid.NewGuid(), _cancellationToken));
+        }
+
+        [Test]
+        public void GivenUnexpectedCode_WhenCreateCommentAsync_ThenShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            _commentRepository
+                .CreateCommentAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<Guid>(), _cancellationToken)
+                .Returns(9999);
+
+            // Act & Assert
+            Assert.ThrowsAsync<InvalidOperationException>(() =>
+                _commentHandler.CreateCommentAsync(Guid.NewGuid(), "Test Content", Guid.NewGuid(), _cancellationToken));
+        }
+    }
+}
---
+++ Forum/Forum.Test/Unit.Tests/Application/TagHandlerTests.cs
@@ -0,0 +1,88 @@
+﻿using Forum.Application.Exceptions;
+using Forum.Application.Handlers;
+using Forum.Domain.Interfaces.Repository;
+using NSubstitute;
+
+namespace Forum.Test.Unit.Tests.Application
+{
+    [TestFixture]
+    public class TagHandlerTests
+    {
+        private ITagRepository _tagRepository;
+        private TagHandler _tagHandler;
+        private CancellationToken _cancellationToken;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _tagRepository = Substitute.For<ITagRepository>();
+            _tagHandler = new TagHandler(_tagRepository, Substitute.For<IPostRepository>());
+            _cancellationToken = CancellationToken.None;
+        }
+
+        [Test]
+        public async Task GivenValidResult_WhenTagPostAsync_ThenShouldCompleteWithoutException()
+        {
+            // Arrange
+            _tagRepository
+                .TagPostAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<Guid>(), _cancellationToken)
+                .Returns(0);
+
+            // Act & Assert
+            Assert.DoesNotThrowAsync(() =>
+                _tagHandler.TagPostAsync(Guid.NewGuid(), "Tech", Guid.NewGuid(), _cancellationToken));
+        }
+
+        [Test]
+        public void GivenPostNotFound_WhenTagPostAsync_ThenShouldThrowPostNotFoundException()
+        {
+            // Arrange
+            _tagRepository
+                .TagPostAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<Guid>(), _cancellationToken)
+                .Returns(1001);
+
+            // Act & Assert
+            Assert.ThrowsAsync<PostNotFoundException>(() =>
+                _tagHandler.TagPostAsync(Guid.NewGuid(), "Tech", Guid.NewGuid(), _cancellationToken));
+        }
+
+        [Test]
+        public void GivenTagNotFound_WhenTagPostAsync_ThenShouldThrowTagNotFoundException()
+        {
+            // Arrange
+            _tagRepository
+                .TagPostAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<Guid>(), _cancellationToken)
+                .Returns(1002);
+
+            // Act & Assert
+            Assert.ThrowsAsync<TagNotFoundException>(() =>
+                _tagHandler.TagPostAsync(Guid.NewGuid(), "Tech", Guid.NewGuid(), _cancellationToken));
+        }
+
+        [Test]
+        public void GivenDuplicateTag_WhenTagPostAsync_ThenShouldThrowDuplicateTagException()
+        {
+            // Arrange
+            _tagRepository
+                .TagPostAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<Guid>(), _cancellationToken)
+                .Returns(1003);
+
+            // Act & Assert
+            Assert.ThrowsAsync<DuplicateTagException>(() =>
+                _tagHandler.TagPostAsync(Guid.NewGuid(), "Tech", Guid.NewGuid(), _cancellationToken));
+        }
+
+        [Test]
+        public void GivenUnexpectedCode_WhenTagPostAsync_ThenShouldThrowInvalidOperationException()
+        {
+            // Arrange
+            _tagRepository
+                .TagPostAsync(Arg.Any<Guid>(), Arg.Any<Guid>(), Arg.Any<string>(), Arg.Any<Guid>(), _cancellationToken)
+                .Returns(9999);
+
+            // Act & Assert
+            Assert.ThrowsAsync<InvalidOperationException>(() =>
+                _tagHandler.TagPostAsync(Guid.NewGuid(), "Tech", Guid.NewGuid(), _cancellationToken));
+        }
+    }
+}
