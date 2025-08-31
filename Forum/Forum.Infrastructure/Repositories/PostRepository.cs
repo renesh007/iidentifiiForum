9e390b4adda086793ebfbd93d56163391da9c41d
renesh007 <r.nandk@yahoo.com>
2025-08-31 20:55:07 +0200

    feat: implement PostRepository

--- Forum/Forum.Infrastructure/Repositories/PostRepository.cs
+++ Forum/Forum.Infrastructure/Repositories/PostRepository.cs
@@ -1,5 +1,7 @@
-﻿using Forum.Domain.Entities;
+﻿using Dapper;
+using Forum.Domain.Entities;
 using Forum.Domain.Interfaces.Repository;
+using System.Data;
 
 namespace Forum.Infrastructure.Repositories
 {
@@ -11,15 +13,43 @@ namespace Forum.Infrastructure.Repositories
             _dbConnectionFactory = dbConnectionFactory;
         }
 
-        public async Task<Guid> CreateAsync(string title, string content, Guid userId, CancellationToken cancellationToken)
+        public async Task<Guid> CreateAsync(Guid postId, string title, string content, Guid userId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            using (IDbConnection connection = _dbConnectionFactory.CreateConnection())
+            {
+                string sql = @"
+                        INSERT INTO dbo.tb_Post (Id, Title, Content, UserId)
+                        VALUES (@PostID, @Title, @Content, @UserID);";
+
+                await connection.ExecuteAsync(
+                       new CommandDefinition(
+                           sql,
+                           new { PostID = postId, Title = title, Content = content, UserID = userId },
+                           cancellationToken: cancellationToken
+                       )
+   );
+                return postId;
+            }
         }
 
         public async Task<Post?> GetByIdAsync(Guid postId, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
-        }
+            using (IDbConnection connection = _dbConnectionFactory.CreateConnection())
+            {
+                string sql = @"
+                        SELECT Id, Title, Content, UserID, CreatedOn
+                        FROM dbo.tb_Post
+                        WHERE Id = @PostID;";
+                var post = await connection.QueryFirstOrDefaultAsync<Post>(
+                       new CommandDefinition(
+                           sql,
+                           new { PostID = postId },
+                           cancellationToken: cancellationToken
+                           )
+                       );
 
+                return post;
+            }
+        }
     }
 }
